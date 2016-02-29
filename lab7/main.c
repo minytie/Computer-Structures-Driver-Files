f5c61303ffde48ff19f447d0ae9ac1705fea5290
Robert Peter Mulligan <rpmullig@phyllite.soic.indiana.edu>
2016-02-29 17:08:28 -0500

    Lab7 finished, still flickering though

--- lab7/main.c
+++ lab7/main.c
@@ -14,6 +14,9 @@
 #include <f3d_lcd_sd.h>
 #include <stdio.h>
 
+//adding an additional library for Math.max
+#include <math.h>
+
 #define TIMER 20000
 
 int main(void) { 
@@ -24,7 +27,7 @@ int main(void) {
   f3d_uart_init();
   f3d_gyro_init();
 
-  printf("passed inits");
+  //printf("passed inits");
   //buffers
   setvbuf(stdin, NULL, _IONBF, 0);
   setvbuf(stdout, NULL, _IONBF, 0);
@@ -32,29 +35,95 @@ int main(void) {
 
   f3d_lcd_fillScreen(BLACK);
 
+  //gryo float data
   float data[3];
+
+  //for the strings that print out
   char data_string_x[30]; 
   char data_string_y[30]; 
   char data_string_z[30]; 
 
   while(1){
+    //collects data and mutates data variable
   f3d_gyro_getdata(data);
 
-  sprintf(data_string_x, "x = %6f", data[0]);
-  sprintf(data_string_y, "y = %6f", data[1]);
-  sprintf(data_string_z, "z = %6f", data[2]);
+  sprintf(data_string_x, "x = %4f", data[0]);
+  sprintf(data_string_y, "y = %4f", data[1]);
+  sprintf(data_string_z, "z = %4f", data[2]);
 
   
-  f3d_lcd_drawString(30,50,data_string_x, WHITE, BLACK);   
-  f3d_lcd_drawString(30,60,data_string_y, WHITE, BLACK);   
-  f3d_lcd_drawString(30,70,data_string_z, WHITE, BLACK);   
+  f3d_lcd_drawString(30,10,data_string_x, BLUE, BLACK);   
+  f3d_lcd_drawString(30,20,data_string_y, YELLOW, BLACK);   
+  f3d_lcd_drawString(30,30,data_string_z, RED, BLACK);   
   
   //bar info by gleb
   
-  //f3d_lcd_setAddrWindow(x1,256-gyrodata,x2,y2,MADCTLGRAPHICS);
-  
+  // f3d_lcd_setAddrWindow(x1,256-gyrodata,x2,y2,MADCTLGRAPHICS);
+  //don't forget to have a threshold for gyrodata.
+  //
+
+  int x = data[0]*2.5; 
+  int y = data[1]*2.5; 
+  int z = data[2]*2.5; 
+
+  int inc, size; 
+
+  for(inc = 69; inc < 98; inc++){
+    if(x>=0){
+      for(size = 64; size < x+64; size++) {
+	f3d_lcd_drawPixel(size,inc,BLUE);
+      }
+    }else{
+      for(size = 64; size > x+64; size--) {
+	f3d_lcd_drawPixel(size,inc,BLUE);
+      }
+    }
 
   }
+
+
+  for(inc = 98; inc < 127; inc++){
+    if(y>=0){
+      for(size = 64; size < y+64; size++) {
+	f3d_lcd_drawPixel(size,inc,YELLOW);
+      }
+    }else{
+      for(size = 64; size > y+64; size--) {
+	f3d_lcd_drawPixel(size,inc,YELLOW);
+      }
+    }
+  }
+
+
+  for(inc = 127; inc < 160; inc++) {
+    if(z>=0){
+      for(size = 64; size < z+64; size++) {
+	f3d_lcd_drawPixel(size,inc,RED);
+      }
+    }else{
+      for(size = 64; size > z+64 && size >= 0; size--) {
+	f3d_lcd_drawPixel(size,inc,RED);
+      }
+
+    }
+
+  }
+
+  int row, col;
+
+  f3d_lcd_setAddrWindow (0,40,ST7735_width-1,ST7735_height-1,MADCTLGRAPHICS);
+  for (row=0;row<ST7735_width;row++) {
+    for (col=0;col<ST7735_height; col++) {
+      f3d_lcd_pushColor(BLACK,1);
+    }
+  }
+
+  
+
+  }//end of while loop
+
+
+  }//end of main function
 
 
   /* 
@@ -78,7 +147,7 @@ int main(void) {
 }
 */
  
-}
+
 
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line) {
